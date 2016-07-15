918767a8328e5d6e89bcc230868a2ad5bacd2f7c
ronyh7 <rony-hernandez_7@hotmail.com>
2016-07-15 00:11:03 -0400

    trabajo en h2

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -11,31 +11,39 @@ import java.util.ArrayList;
  * Created by Rony on 5/29/2016.
  */
 public class Database {
-     private  static Server server=null;
+     private static Server server = null;
      private static Connection conexion=null;
      private static Database database=null;
 
     public Database() throws SQLException {
     }
-
+    //conexion = DriverManager.getConnection("jdbc:h2:tcp:~/practica2", "sa", "1234");
 
     public static Database getDatabase() throws SQLException{
         if(database==null || server==null){
             database= new Database();
             try {
-                server = Server.createTcpServer("jdbc:h2:tcp:~/practica2").start();
+                server = Server.createTcpServer("-tcpAllowOthers").start();
                 Class.forName("org.h2.Driver");
-            } catch (ClassNotFoundException e) {
+                conexion = DriverManager.
+                        getConnection("jdbc:h2:tcp://localhost/~/practica2", "sa", "1234");
+                System.out.println("Connection Established: "
+                        + conexion.getMetaData().getDatabaseProductName() + "/" + conexion.getCatalog());
+
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-            conexion = DriverManager.getConnection("jdbc:h2:tcp:~/practica2", "sa", "1234");
+
         }
         return database;
     }
 
+
+
+
     public void insert(String[] params) throws SQLException {
 
-        String insert="INSERT INTO ESTUDIANTES(MATRICULA,NOMBRE,APELLIDOS,TELEFONO) VALUES(";
+        String insert="INSERT INTO ESTUDIANTE(MATRICULA,NOMBRE,APELLIDOS,TELEFONO) VALUES(";
         for(int i=0;i<params.length;i++){
             if(i!=params.length-1)
                 insert+="'"+params[i]+"'"+",";
@@ -45,13 +53,14 @@ public class Database {
         conexion.createStatement().execute(insert);
     }
 
+
     public void delete(String Matricula) throws SQLException {
-        String delete="DELETE FROM ESTUDIANTES WHERE MATRICULA="+"'"+Matricula+"'";
+        String delete="DELETE FROM ESTUDIANTE WHERE MATRICULA="+"'"+Matricula+"'";
         conexion.createStatement().execute(delete);
     }
 
     public void update(String[] estudiante) throws SQLException {
-        String update=String.format("UPDATE ESTUDIANTES SET NOMBRE = '%s', APELLIDOS = '%s', TELEFONO = '%s'" +
+        String update=String.format("UPDATE ESTUDIANTE SET NOMBRE = '%s', APELLIDOS = '%s', TELEFONO = '%s'" +
                         " WHERE MATRICULA = '%s'",
                 estudiante[1], estudiante[2], estudiante[3], estudiante[0]);
         conexion.createStatement().execute(update);
@@ -59,7 +68,7 @@ public class Database {
 
     public ArrayList<Estudiante> select() throws SQLException {
         ArrayList<Estudiante> lista = new ArrayList<Estudiante>();
-        String select="SELECT * FROM ESTUDIANTES";
+        String select="SELECT * FROM ESTUDIANTE";
         ResultSet r= conexion.createStatement().executeQuery(select);
         while(r.next()){
             Estudiante e = new Estudiante();
